8d875a0a9c4aff1f167b463611e8ba1b93840e1b
“Joshua <joshuaereyes@cpp.edu>
2023-04-25 17:10:39 -0700

    added main file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.cs2450_final_project {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.cs2450_final_project to javafx.fxml;
     exports com.example.cs2450_final_project;
 }
